b6816a30166cae363401120bce7c62641588684b
jinsheng.wu2 <jinsheng.wu2@zhenai.com>
2019-06-24 10:38:17 +0800

    add

---
+++ app/src/main/java/com/salton123/codelimit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.salton123.codelimit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
